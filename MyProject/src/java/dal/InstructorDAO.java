986df26ca8d1546b87320c3918a56ed2b4098748
Sondeptrai0606 <sonnl06062001@gmail.com>
2022-10-24 23:26:17 +0700

    time table

---
+++ MyProject/src/java/dal/InstructorDAO.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import model.Instructor;
+
+/**
+ *
+ * @author win
+ */
+public class InstructorDAO extends DBContext{
+
+    public ArrayList<Instructor> getAllInstructor() {
+        ArrayList<Instructor> ilist = new ArrayList<>();
+        String sql = "  select * from Instructor";
+        try {
+            PreparedStatement stm = connection.prepareStatement(sql);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Instructor i = new Instructor(rs.getInt(1), rs.getString(2), rs.getString(3));
+                ilist.add(i);
+            }
+        } catch (Exception e) {
+        }
+        return ilist;
+    }
+}
---
+++ MyProject/src/java/dal/StudentDAO.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import model.Student;
+
+/**
+ *
+ * @author win
+ */
+public class StudentDAO extends DBContext {
+
+    public ArrayList<Student> getAllStudent(String groupcode) {
+        ArrayList<Student> alist = new ArrayList<>();
+        String sql = " select s.* , g.* from Student s, [Group] g, Enroll e\n"
+                + "  where s.SID = e.SID and g.GroupID = e.GroupID and g.GroupCode like ?";
+        try {
+            PreparedStatement stm = connection.prepareStatement(sql);
+            stm.setString(1, "%" + groupcode + "%");
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Student s = new Student(rs.getInt(1), rs.getString(2), rs.getString(3),
+                        rs.getBoolean(4), rs.getString(5));
+                alist.add(s);
+            }
+        } catch (Exception e) {
+        }
+        return alist;
+    }
+
+}
