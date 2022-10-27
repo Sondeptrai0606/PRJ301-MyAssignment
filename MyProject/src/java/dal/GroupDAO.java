649b44c4415cc7986bcd1314dcab967302a8f1a7
Sondeptrai0606 <sonnl06062001@gmail.com>
2022-10-27 23:51:26 +0700

    ok

--- MyProject/src/java/dal/GroupDAO.java
+++ MyProject/src/java/dal/GroupDAO.java
@@ -30,49 +30,7 @@ public class GroupDAO extends DBContext {
 
     }
 
-    public ArrayList getAllDep(String term) {
-        ArrayList camp = new ArrayList();
-        if (term == null) {
-            term = "";
-        }
-        try {
-            String sql = "   select  distinct Department from [Group] where Term like ?";
-            PreparedStatement stm = connection.prepareStatement(sql);
-            stm.setString(1, "%" + term + "%");
-
-            ResultSet rs = stm.executeQuery();
-            while (rs.next()) {
-                camp.add(rs.getString(1));
-            }
-        } catch (Exception e) {
-        }
-        return camp;
-
-    }
-
-    public ArrayList getAllSubject(String term, String dept) {
-        ArrayList Subject = new ArrayList();
-        if (term == null) {
-            term = "";
-        }
-        if (dept == null) {
-            dept = "";
-        }
-        try {
-            String sql = "    select  distinct g.SubID, s.* from [Group] g, [Subject] s\n"
-                    + " where g.SubID = s.SubID and g.Department like ? and  Term like ?";
-            PreparedStatement stm = connection.prepareStatement(sql);
-            stm.setString(1, "%" + dept + "%");
-            stm.setString(2, "%" + term + "%");
-            ResultSet rs = stm.executeQuery();
-            while (rs.next()) {
-                Subject.add(rs.getString(4) );
-            }
-        } catch (Exception e) {
-        }
-        return Subject;
-    }
-
+ 
     public ArrayList getAllGroup(String term, String dept, String subjectid) {
         if (term == null) {
             term = "";
